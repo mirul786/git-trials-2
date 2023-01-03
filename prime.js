f34844e136463f2e6d9d06b08d757b6558eeaddd
mirul786 <mirulkhan786@gmail.com>
2023-01-03 15:20:32 +0530

    string

---
+++ prime.js
@@ -0,0 +1,14 @@
+// Given an array find out the number of elements for which both the neighbors are greater than the elements
+
+let arr = [5, 7, 6, 9, 8, 10]
+
+// outpust--> 2 // (6 and 8)
+
+let empArr= []
+let count = 0;
+for (let i=1; i<arr.length; i++){
+  if(arr[i]<arr[i-1] && arr[i]<arr[i+1]){
+    count++
+  empArr.push(arr[i])
+  }
+}
